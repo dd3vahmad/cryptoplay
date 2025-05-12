40064c0bb7df4a1bc6ad380ee47895e36994d7d9
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-05-12 21:51:26 +0100

    Fix: file structure and add starting hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Navbar from "./components/Navbar";
+import Hero from "@/components/Hero";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <main className="px-16  py-5">Hero Section</main>;
+};
+
+export default Hero;
