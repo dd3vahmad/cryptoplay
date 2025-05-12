676a322e1d858053915e19c6c8898c8fc4e05458
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-05-12 22:44:38 +0100

    Add: image assets and design a section of the landing page hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
 import React from "react";
+import heroBanner from "@/assets/hero_banner.png";
+import { Button } from "./ui/button";
+import { Trophy } from "lucide-react";
 
 const Hero = () => {
-  return <main className="px-16  py-5">Hero Section</main>;
+  return (
+    <main className="px-16 py-5 flex flex-col gap-y-3">
+      <div className="grid grid-cols-7 gap-x-3 flex-1">
+        <div className="col-span-5 relative">
+          <div className="flex flex-col gap-y-8 absolute z-20 bg-transparent h-full w-full items-center justify-center left-0 top-0">
+            <h1 className="text-5xl font-bold text-white">
+              Play to <span className="text-tertiary">Earn</span> in Style
+            </h1>
+
+            <p className="text-sm text-white max-w-lg text-center">
+              Experience the future of online gaming with CryptoPlay. Connect
+              your wallet and start winning today.
+            </p>
+
+            <div className="flex items-center gap-3">
+              <Button className="bg-tertiary text-white">Start Playing</Button>
+              <Button className="bg-transparent text-tertiary border border-tertiary">
+                <Trophy /> View Games
+              </Button>
+            </div>
+          </div>
+
+          <span className="z-10 absolute bg-black w-[320px] h-[320px] top-[30%] blur-3xl left-[40%]"></span>
+
+          <div style={{ position: "relative", height: "60vh", width: "100%" }}>
+            <Image
+              src={heroBanner.src}
+              alt="Hero section banner"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div></div>
+    </main>
+  );
 };
 
 export default Hero;
